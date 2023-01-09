3396c335135f6e8b7c03e025fbfa4ff16856bf88
Ismi-Dzikrina <ismidzikrina@gmail.com>
2023-01-09 14:24:05 +0700

    inisialisasi project

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,107 @@
+<template>
+<head>
+    <title>Login Account - Employee Database Application</title>
+</head>
+
+<div class="auth-wrapper">
+        <div class="auth-content">
+            <div class="auth-bg">
+                <span class="r"></span>
+                <span class="r s"></span>
+                <span class="r s"></span>
+                <span class="r"></span>
+            </div>
+            <div class="card">
+                <div class="card-body text-center">
+                    <div class="mb-4">
+                        <i class="feather icon-unlock auth-icon"></i>
+                    </div>
+                    <h3 class="mb-4">Login</h3>
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" v-model="form.username" :class="{ 'is-invalid': errors.username }"  placeholder="Username">
+                    </div>
+                    <div class="input-group mb-4">
+                        <input v-if="showPassword" type="text" class="form-control" v-model="form.password" :class="{ 'is-invalid': errors.password }" placeholder="Password"/>
+                        <input v-else type="password" class="form-control" v-model="form.password" :class="{ 'is-invalid': errors.password }" placeholder="Password">
+                        <div class="input-group-append" style="cursor:pointer">
+                            <a class="input-group-text" @click="toggleShow">
+                                <span class="icon is-small is-right">
+                                    <i class="fas" :class="{ 'fa-eye-slash': showPassword, 'fa-eye': !showPassword }"></i>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary shadow-2 mb-4">Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    //import layout
+    import LayoutAuth from '../../Layouts/Auth.vue';
+    //import reactive
+    import { onMounted, reactive, ref } from 'vue';
+    //import inertia adapter
+    import { Inertia } from '@inertiajs/inertia';
+    //import Head and useform from inertia
+    import { Head, Link } from '@inertiajs/inertia-vue3';
+
+    export default {
+        //layout
+        layout: LayoutAuth,
+        //register component
+        components: {
+            Head,
+            Link
+        },
+        props: {
+            errors: Object,
+            session: Object
+        },
+
+        //define composition API
+        setup() {
+            const showPassword = ref(false);
+
+            onMounted(() => {
+                return (showPassword.value) ? "Hide" : "Show";
+            })
+            //show hide password
+            const toggleShow = () => {
+                showPassword.value = !showPassword.value;
+            }
+
+            //define form state
+            const form = reactive({
+                username: '',
+                password: '',
+            });
+
+            //submit method
+            const submit = () => {
+
+                //send data to server
+                Inertia.post('/login', {
+
+                    //data
+                    username: form.username,
+                    password: form.password,
+                });
+            }
+
+            //return form state and submit method
+            return {
+                form,
+                toggleShow, showPassword,
+                submit,
+            };
+        }
+
+    }
+</script>
+
+<style>
+</style>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+        <script src="{{ mix('/js/app.js') }}" defer></script>
+        <link rel="icon" href="{{ asset('/images/favicon.ico') }}" type="image/ico" />
+        <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+        <link rel="stylesheet" href="{{ asset('/fonts/fontawesome/css/fontawesome-all.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('/plugins/animation/css/animate.min.css') }}">
+        <script src="{{ asset('/js/vendor-all.min.js') }}"></script>
+        <script src="{{ asset('/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+    </head>
+    <body>
+        @inertia
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
+</html>
